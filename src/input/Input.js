b967bd33fd70de055a1e4e2db4cb44a70a38f79a
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-06 16:10:17 +0700

    fix: mouse position breaks when unfocus window

--- src/input/Input.js
+++ src/input/Input.js
@@ -1,3 +1,4 @@
+import Vector2 from "../types/Vector2.js";
 class Input {
     static init() {
         const keyUpHandler = (event) => {
@@ -6,8 +7,13 @@ class Input {
         const keyDownHandler = (event) => {
             Input.lastKeyDownKey = event.key;
         };
+        const logMousePosition = (event) => {
+            if (event)
+                Input.lastMousePosition = new Vector2(event.clientX, event.clientY);
+        };
         document.addEventListener('keyup', keyUpHandler, false);
         document.addEventListener('keydown', keyDownHandler, false);
+        document.onmousemove = logMousePosition;
     }
     static resetInput() {
         Input.lastKeyUpKey = "";
@@ -18,6 +24,12 @@ class Input {
     }
     static getKeyUp(key) {
         return Input.lastKeyUpKey == key;
+    }
+    static getMousePosition() {
+        if (this.lastMousePosition)
+            return this.lastMousePosition;
+        else
+            return Vector2.zero();
     }
 }
 export default Input;
--- src/input/Input.ts
+++ src/input/Input.ts
@@ -1,25 +1,33 @@
 ﻿import Game from "../system/Game.js"
 import Debug from "../system/Debug.js"
+import Vector2 from "../types/Vector2.js"
 
 class Input
 {
     private static lastKeyUpKey: string
     private static lastKeyDownKey: string
+    private static lastMousePosition: Vector2
 
     public static init(): void
     {
-        const keyUpHandler = (event: KeyboardEvent) =>
+        const keyUpHandler = (event: KeyboardEvent): void =>
         {
             Input.lastKeyUpKey = event.key
         }
 
-        const keyDownHandler = (event: KeyboardEvent) =>
+        const keyDownHandler = (event: KeyboardEvent): void =>
         {
             Input.lastKeyDownKey = event.key
         }
 
+        const logMousePosition = (event: MouseEvent): void =>
+        {
+            if (event) Input.lastMousePosition = new Vector2(event.clientX, event.clientY)
+        }
+
         document.addEventListener('keyup', keyUpHandler, false)
         document.addEventListener('keydown', keyDownHandler, false)
+        document.onmousemove = logMousePosition
     }
 
     public static resetInput(): void
@@ -36,7 +44,14 @@ class Input
     public static getKeyUp(key: string): boolean
     {
         return Input.lastKeyUpKey == key
+    }
 
+    public static getMousePosition(): Vector2
+    {
+        if (this.lastMousePosition)
+            return this.lastMousePosition
+        else
+            return Vector2.zero()
     }
 }
 
