b320a3826a0c4f85b1e6b1ce8a4ab45dcc48d74f
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-06 09:42:50 +0700

    first commit!

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+
+const app = express();
+app.use(express.static('./src'));
+
+app.get('/', (req, res)=>{
+    res.sendFile('index.html', {root: __dirname});
+    // startGame();
+})
+
+const PORT = 1234
+app.listen(PORT, () =>{
+    console.log(`Sever is running on port ${PORT}`);
+})
