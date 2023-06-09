6bb39978847390b42b7b24040c643db5cd6c50a6
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-09 11:52:37 +0700

    fix: bird stucks in walls

--- src/game/play/VerticalWall.ts
+++ src/game/play/VerticalWall.ts
@@ -7,7 +7,6 @@ import GameState from "../GameState"
 
 class VerticalWall extends Wall
 {
-
     constructor(name: string) {
         super(name)
         BirdGame.gameStateChanged.subscribe(this.stateChangedHandler.bind(this))
@@ -28,6 +27,7 @@ class VerticalWall extends Wall
     }
 
     public showSpike(): void {
+        this.collider.isActive = true
         const spikeCount = this.difficultyFunction(BirdGame.currentScore)
         const spikeIndices = Maths.randomIntBag(0, this.spikes.length, spikeCount)
         for (let i = 0; i < spikeIndices.length; i++)
@@ -37,6 +37,7 @@ class VerticalWall extends Wall
     }
 
     public hideSpike(): void {
+        this.collider.isActive = false
         for (let i = 0; i < this.spikes.length; i++)
         {
             this.spikes[i].hide()
@@ -47,10 +48,12 @@ class VerticalWall extends Wall
         if (gameState === GameState.WELCOME)
         {
             this.hideSpike()
+            this.showSpike()
         }
     }
 
     private difficultyFunction(x: number): number {
+        if (x == 0) return 0
         return Math.floor(Math.log(x + 2) / Math.log(1.6) * 0.8)
     }
 }
