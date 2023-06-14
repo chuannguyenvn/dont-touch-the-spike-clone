ea9c3e14a4cf26db89a42bfbc7e8b3c253b6ee8f
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-14 16:14:28 +0700

    feat: tween score result background

--- src/game/result/ResultBackground.ts
+++ src/game/result/ResultBackground.ts
@@ -7,6 +7,7 @@ import Vector from '../../engine/math/Vector'
 import Color from '../../engine/math/Color'
 import BirdGame from '../BirdGame'
 import GameState from '../GameState'
+import Ease from '../../engine/utility/tween/Ease'
 
 class ResultBackground extends Node {
     public transform: Transform
@@ -16,7 +17,8 @@ class ResultBackground extends Node {
         super(name)
 
         this.transform = this.addComponent(ComponentType.TRANSFORM) as Transform
-
+        this.transform.scale = new Vector(1, 0)
+        
         const rectangle = new RectangleShape(new Vector(400, 150), new Color(0.5, 0.5, 0.5, 0.5))
         this.renderer = this.addComponent(ComponentType.RENDERER) as Renderer
         this.renderer.setDrawable(rectangle)
@@ -24,15 +26,18 @@ class ResultBackground extends Node {
 
         this.isVisible = false
         this.isActive = false
-        
+
         BirdGame.stateMachine.configure(GameState.RESULT).onEntry(this.getGuid(), () => {
             this.isVisible = true
             this.isActive = true
+            this.transform.tweenScale(Vector.ONE, 0.5, 0, Ease.OUT_CUBIC, false)
         })
 
         BirdGame.stateMachine.configure(GameState.RESULT).onExit(this.getGuid(), () => {
-            this.isVisible = false
-            this.isActive = false
+            this.transform.tweenScale(new Vector(1, 0), 0.5, 0, Ease.IN_CUBIC, false, () => {
+                this.isVisible = false
+                this.isActive = false
+            })
         })
     }
 }
