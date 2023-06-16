a65cec0e7fa7060c1a5799404b1b18764766e8e9
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-16 15:43:13 +0700

    fix: score and score background shows/hides incorrectly

--- src/game/play/ScoreBackground.ts
+++ src/game/play/ScoreBackground.ts
@@ -24,12 +24,12 @@ class ScoreBackground extends Node {
         this.isVisible = false
         this.isActive = false
 
-        BirdGame.stateMachine.configure(GameState.WELCOME).onExit(this.getGuid(), () => {
+        BirdGame.stateMachine.configure(GameState.PLAY).onEntry(this.getGuid(), () => {
             this.isVisible = true
             this.isActive = true
         })
 
-        BirdGame.stateMachine.configure(GameState.RESULT).onEntry(this.getGuid(), () => {
+        BirdGame.stateMachine.configure(GameState.RESULT).onExit(this.getGuid(), () => {
             this.isVisible = false
             this.isActive = false
         })
